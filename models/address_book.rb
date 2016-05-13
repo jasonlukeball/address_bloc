66a210d2f64d3607dd5426d245d2b6cc8b9b7c1a
Jason Ball <jasonlukeball@me.com>
2016-05-13 19:18:09 +1000

    Add #binary search and specs

--- models/address_book.rb
+++ models/address_book.rb
@@ -10,6 +10,7 @@ class AddressBook
   end
 
   def add_entry(name, phone_number, email)
+    # inserts entries in alphabetical order by name
     index = 0
     entries.each do |entry|
       if name < entry.name
@@ -31,6 +32,28 @@ class AddressBook
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+
+  end
+
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      mid = ( lower + upper ) / 2
+      mid_name = entries[mid].name
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+
+    end
+
+    nil
 
   end
 
