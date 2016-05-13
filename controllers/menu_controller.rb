b886bcadf66dfcc671cb0b4bcee8941498cc42f5
Jason Ball <jasonlukeball@me.com>
2016-05-13 22:34:21 +1000

    Add read_csv, search_entries, edit_entry, delete_entry

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -18,6 +18,7 @@ class MenuController
     puts "4 - Search for an entry"
     puts "5 - Import entries from a CSV"
     puts "6 - Exit"
+    puts
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -91,7 +92,10 @@ class MenuController
     case selection
       when "n"
       when "d"
+        delete_entry(entry)
       when "e"
+        edit_entry(entry)
+        entry_submenu(entry)
       when "m"
         system "clear"
         main_menu
@@ -118,12 +122,97 @@ class MenuController
   end
 
 
+  def edit_entry(entry)
+
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+
+    puts "Updated entry: \n\n"
+    puts entry
+
+  end
+
+
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+
+    match = address_book.binary_search(name)
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
+
+  end
+
+
+  def search_submenu(entry)
+    puts
+    puts "d - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    selection = gets.chomp
+
+    case selection
+      when "d"
+        system "clear"
+        delete_entry(entry)
+        main_menu
+      when "e"
+        edit_entry(entry)
+        system "clear"
+        main_menu
+      when "m"
+        system "clear"
+        main_menu
+      else
+        system "clear"
+        puts "#{selection} is not a valid input"
+        puts entry.to_s
+        search_submenu(entry)
+    end
 
   end
 
 
   def read_csv
+
+    print "Enter CSV filename to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = address_book.import_from_csv(file_name).count
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "Error! #{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
 
   end
 
